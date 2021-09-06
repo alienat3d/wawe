590d6d6312067c9fe1617dd29e51678c8021883a
alienat3d <psy.alienated@gmail.com>
2021-09-06 12:32:37 +0300

    header and hero sect finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,93 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+const { src, dest, watch, parallel, series }  = require('gulp');
 
-const scss = require("gulp-sass");
-const concat = require("gulp-concat");
-const autoprefixer = require("gulp-autoprefixer");
-const uglify = require("gulp-uglify");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const browserSync = require("browser-sync").create();
+const scss          = require('gulp-sass');
+const concat        = require('gulp-concat');
+const browserSync   = require('browser-sync').create();
+const uglify        = require('gulp-uglify-es').default;
+const autoprefixer  = require('gulp-autoprefixer');
+const imagemin      = require('gulp-imagemin');
+const del           = require('del');
 
 function browsersync() {
   browserSync.init({
-    server: {
-      baseDir: "app/",
+    server : {
+      baseDir: 'app/'
     },
-    notify: false,
+    notify: false
   });
 }
 
-function styles() {
-  return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(concat("style.min.css"))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ["last 10 versions"],
-        grid: true,
-      })
-    )
-    .pipe(dest("app/css"))
-    .pipe(browserSync.stream());
-}
-
-function scripts() {
-  return src(["app/js/main.js"])
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("app/js/"))
-    .pipe(browserSync.stream());
+function cleanDist() {
+  return del('dist')
 }
 
 function images() {
-  return src("app/images/**/*")
+  return src('app/images/**/*')
     .pipe(imagemin(
       [
         imagemin.gifsicle({ interlaced: true }),
         imagemin.mozjpeg({ quality: 75, progressive: true }),
         imagemin.optipng({ optimizationLevel: 5 }),
         imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-        }),
-      ])
-    )
-    .pipe(dest("dist/images"));
+          plugins: [
+            { removeViewBox: true },
+            { cleanupIDs: false }
+          ]
+        })
+      ]
+    ))
+    .pipe(dest('dist/images'))
+}
+
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
+
+
+function styles() {
+  return src('app/scss/style.scss')
+      .pipe(scss({outputStyle: 'compressed'}))
+      .pipe(concat('style.min.css'))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true
+      }))
+      .pipe(dest('app/css'))
+      .pipe(browserSync.stream())
 }
 
 function build() {
-  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
-    base: "app",
-  }).pipe(dest("dist"));
-}
-
-function cleanDist() {
-  return del("dist");
+  return src([
+    'app/css/style.min.css',
+    'app/fonts/**/*',
+    'app/js/main.min.js',
+    'app/*.html'
+  ], {base: 'app'})
+    .pipe(dest('dist'))
 }
 
 function watching() {
-  watch(["app/scss/**/*.scss"], styles);
-  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
-exports.scripts = scripts;
-exports.browsersync = browsersync;
 exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
 exports.images = images;
 exports.cleanDist = cleanDist;
 
-exports.default = parallel(styles, scripts, browsersync, watching);
-exports.build = series(cleanDist, images, build);
 
-// exports.build = series(cleanDist, images, build);  //replace
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles ,scripts ,browsersync, watching);
+
+
---
+++ readme.txt
@@ -0,0 +1,11 @@
+Roboto Condensed
+  700 font-family: 'RobotoCondensed-Bold';
+  400 font-family: 'RobotoCondensed-Regular';
+  400i font-family: 'RobotoCondensed-Italic';
+  300 font-family: 'RobotoCondensed-Light';
+  300i font-family: 'RobotoCondensed-LightItalic';
+
+---  
+Raleway
+  700 font-family: 'Raleway-Bold';
+  600 font-family: 'Raleway-SemiBold';
